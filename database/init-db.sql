81b2d52aafd156bdde961b4f920e69365e191ccb
Marcos Daniel da Silva Lima <marcos.d.s.lima1408@gmail.com>
2026-04-24 22:14:48 -0300

    refact: Corrigindo docker-compose.yml e init-db.sql.

--- database/init-db.sql
+++ database/init-db.sql
@@ -1,1 +1,22 @@
-CREATE DATABASE IF NOT EXISTS usuarios DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE TABLE cargos (
+	id_cargo SERIAL PRIMARY KEY,
+	descricao TEXT NOT NULL,
+    is_ativo BOOLEAN DEFAULT TRUE
+);
+
+CREATE TABLE usuarios (
+    id_usuario SERIAL PRIMARY KEY,
+    nome TEXT NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    id_cargo INT,
+    senha_hash TEXT NOT NULL,
+	is_ativo BOOLEAN DEFAULT TRUE,
+    CONSTRAINT fk_usuarios_cargos 
+        FOREIGN KEY (id_cargo) 
+        REFERENCES cargos (id_cargo) 
+        ON DELETE SET NULL
+);
+
+INSERT INTO cargos (descricao) VALUES ('Administrador');
+INSERT INTO cargos (descricao) VALUES ('Operador');
+INSERT INTO cargos (descricao) VALUES ('Gerente');
